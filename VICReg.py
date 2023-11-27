459de89677b1828bba0fb75708e5d99c363ee221
Anthony Zalev <anthony_zalev@hotmail.com>
2023-11-27 16:26:13 -0500

    changes without the data file

--- VICReg.py
+++ VICReg.py
@@ -3,12 +3,25 @@ from torchvision.models import resnet50, ResNet50_Weights
 from torchvision.transforms import v2
 import torch
 import torch.nn as nn
-
-class VICReg():
-    def __init__():
-        pass
+import torch.optim as optimizers
+class VICReg(nn.Module):
     
-    def augment():
+    def __init__(self):
+        self.lamb = 25 # invar loss weight
+        self.mu = 25 # var loss weight
+        self.nu = 1 # covar loss weight
+        self.input_vector_size = 32 * 32
+        self.expander_vector_size = 500
+        self.encoder_vector_size = 400
+        self.encoder = ResNet50_Weights.DEFAULT.transforms()
+        self.expander = Expander(input_size=self.encoder_vector_size, output_size=self.expander_vector_size)
+        self.optimizer = optimizers.SGD
+        self.train_epochs = 50
+        self.batch_size = 256
+        self.base_lr = .01
+        self.lr = (self.batch_size / 256) * self.base_lr# Batch_size / 256 * base_lr
+    
+    def augment(self):
         """
         1. Random Cropping with size ratio between .08 and 1.0 with resizing. RandomResizedCrop(32, scale=(0.08, 0.1)) in PyTorch.
         2. Random horizontal flip with probability 0.5.
@@ -46,46 +59,98 @@ class VICReg():
             v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
         
+    def calculate_loss(self, z, z_prime):
+        """Calculate the loss function. 
+        
+        The following is heavily based on the psuedo code provided on page 13 of https://arxiv.org/pdf/2105.04906.pdf
+        
+        Need to calculate 3 things: 
+        
+        1. Variance
+        
+        2. Invariance
+        
+        3. Covariance
+        
+        Args:
+            z (_type_): batch of images transformed, encodeded, projected
+            z_prime (_type_): batch of images transformed, encodeded, projected
+        """
+        
+        # 1. Variance Loss
+        var_epsilon = 1e-4
+        std_z = torch.sqrt(z.var(dim=0) + var_epsilon)
+        std_z_prime = torch.sqrt(z_prime.var(dim=0) + var_epsilon)
+        std_loss = torch.mean(nn.relu(1 - std_z)) + torch.mean(nn.relu(1 - std_z_prime))
         
         
-    def get_loss(x, self):
+        # 2. Invariance Loss (Just MSE Loss)
+        invar_loss = nn.MSELoss(z, z_prime)
+        
+        # 3. Covariance Loss
+        
+        N , D = z.shape
+        z = z - z.mean(dim=0)
+        z_prime = z_prime - z_prime.mean(dim=0)
+        cov_z = (z.T @ z) / (N - 1)
+        cov_z_prime = (z_prime.T @ z_prime) / (N - 1)
+        
+        cov_z = cov_z.pow(2)
+        cov_z_prime = cov_z_prime.pow(2)
+        
+        loss_c_a = (cov_z.sum() - cov_z.diagonal().sum()) / D
+        loss_c_b = (cov_z_prime.sum() - cov_z_prime.diagonal().sum()) / D
+        
+        loss_cov = loss_c_a + loss_c_b
+        
+        loss = self.lamb * invar_loss + self.mu * std_loss + self.nu * loss_cov
+        
+        return loss
+    
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.expander(x)
+        
+        return x  
+    
+class Expander(nn.Module):
+    def __init__(self, input_size, output_size=8192):
         """
-            # f: encoder network, lambda, mu, nu: coefficients of the
-            invariance, variance and covariance losses, N: batch size
-            , D: dimension of the representations
-            # mse_loss: Mean square error loss function, off_diagonal:
-            off-diagonal elements of a matrix, relu: ReLU activation
-            function
+        
+        expander hφ:
+        Composed of two fully-connected layers with batch normalization and ReLU,
+        and a third linear layer. The sizes of all 3 layers were set to 8192
+
+        Args:
+            input_size (int): cifar vector size 
+            output_size (int): output vector size, also size of intermediate linear layers.
         """
-        # Initialize the Weight Transforms
-        weights = ResNet50_Weights.DEFAULT
-        f_encoder = weights.transforms()
+        super(Expander, self).__init__()
 
+        # First fully-connected layer
+        self.fc1 = nn.Linear(input_size, output_size)
+        self.bn1 = nn.BatchNorm1d(output_size)
+        self.relu1 = nn.ReLU()
 
-        for x in loader: # load a batch with N samples
-            # two randomly augmented versions of x
-            x_a, x_b = self.augment(x)
-            # compute representations
-            z_a = f_encoder(x_a) # N x D
-            z_b = f_encoder(x_b) # N x D
-            # invariance loss
-            sim_loss = nn.MSELoss(z_a, z_b)
-            # variance loss
-            std_z_a = torch.sqrt(z_a.var(dim=0) + 1e-04)
-            std_z_b = torch.sqrt(z_b.var(dim=0) + 1e-04)
-            std_loss = torch.mean(nn.ReLU(1 - std_z_a)) + torch.mean(nn.ReLU(1 - std_z_b))
-            # covariance loss
-            z_a = z_a - z_a.mean(dim=0)
-            z_b = z_b - z_b.mean(dim=0)
-            cov_z_a = (z_a.T @ z_a) / (N - 1)
-            cov_z_b = (z_b.T @ z_b) / (N - 1)
-            cov_loss = off_diagonal(cov_z_a).pow_(2).sum() / D
-            + off_diagonal(cov_z_b).pow_(2).sum() / D
-            # loss
-            loss = lambda * sim_loss + mu * std_loss + nu * cov_loss
-            # optimization step
-            loss.backward()
-            optimizer.step()
+        # Second fully-connected layer
+        self.fc2 = nn.Linear(output_size, output_size)
+        self.bn2 = nn.BatchNorm1d(output_size)
+        self.relu2 = nn.ReLU()
 
+        # Third linear layer
+        self.fc3 = nn.Linear(output_size, output_size)
 
+    def forward(self, x):
+        # Forward pass through the layers
+        x = self.fc1(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
 
+        x = self.fc2(x)
+        x = self.bn2(x)
+        x = self.relu2(x)
+
+        x = self.fc3(x)
+
+        return x
+   
